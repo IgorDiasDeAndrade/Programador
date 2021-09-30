913311a83d0d6f3a39765a5c44c8ee955c1dbfc0
Igor Dias de Andrade <Igor.d.andrade@live.com>
2021-09-30 20:15:19 -0300

    teste 2

--- tryexcept.py
+++ tryexcept.py
@@ -1,1 +1,36 @@
+#varstringpadrao = 'oi igor'
+#try:
+#    inteirostring = int(varstringpadrao)
+#except:
+#    inteirostring = -1
+#
+#print ('primeiro', inteirostring)
+#
+#astr = '123'
+#try:
+#    istr = int(astr)
+#except:
+#    istr = -1
+
+#print('segundo', istr)
+#entrada = input('Entre com um número:')
+#try:
+#    valorinteiro = int(entrada)
+#except:
+#    valorinteiro = -1
+
+##    print("Bom trabalho")
+#else:
+#    print('Não é um numero')
+i = 0
+while i <= 9:
+    i = i +1
     print(i)
+
+def coisa():
+    for i in range(10):
+        print(i)
+
+coisa()
+
+coisa()
